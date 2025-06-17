2ccd44b21c65e4357511373e70b85291e480f878
Enrique Melgar <j_enriquem@hotmail.com>
2025-06-17 17:52:31 -0600

    first commit

---
+++ docs/story_content/triggers.js
@@ -0,0 +1,38 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5ed7GBzBLtX":
+        Script1();
+        break;
+      case "6ipjmbPP5xV":
+        Script2();
+        break;
+      case "6l6tuBjQDeC":
+        Script3();
+        break;
+      case "6QPa7AnbfKW":
+        Script4();
+        break;
+      case "6JNdFmELrcC":
+        Script5();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
