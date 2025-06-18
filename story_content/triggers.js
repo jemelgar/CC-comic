1ba5655331ecad44fabe6ee4eac0e475e6dfe7c9
jemelgar <84332715+jemelgar@users.noreply.github.com>
2025-06-18 00:49:05 +0000

    Deploy Storyline to main [skip ci]

---
+++ story_content/triggers.js
@@ -0,0 +1,38 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5l1Nz0S0mHN":
+        Script1();
+        break;
+      case "6ULzbkoV7tA":
+        Script2();
+        break;
+      case "5xr7YCf40rL":
+        Script3();
+        break;
+      case "69TcalFvCCJ":
+        Script4();
+        break;
+      case "6HHe8k4Qhxp":
+        Script5();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
