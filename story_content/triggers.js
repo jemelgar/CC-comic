cee19e962fd0567b4c29b39603bd9a8cbe6c00dd
Enrique Melgar <j_enriquem@hotmail.com>
2025-06-18 08:13:05 -0600

    updated 2025-06-18

---
+++ story_content/triggers.js
@@ -0,0 +1,38 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6isoQIGsAfE":
+        Script1();
+        break;
+      case "5sAtCWFztDu":
+        Script2();
+        break;
+      case "6bFuO2z2a5p":
+        Script3();
+        break;
+      case "6GNQfarrzGL":
+        Script4();
+        break;
+      case "6VOh6Jf8xA4":
+        Script5();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
